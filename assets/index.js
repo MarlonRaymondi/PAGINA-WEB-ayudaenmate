d945f60d16706d19abf93e177bf2c278ea99779f
AngelDevelopment <raymondi.diaz.angel.aaron@gmail.com>
2023-05-20 12:26:46 -0500

    Más detalles y redes sociales

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,9 @@ const details = document.querySelectorAll('section.course-details .container-gri
 const pricing = document.querySelector('#details');
 const section = document.querySelector('section.payment');
 
+const instagram = document.querySelector('.social-medias .instagram');
+const facebook = document.querySelector('.social-medias .facebook');
+
 courses.forEach(course => course.addEventListener('click', () => {
   pricing.scrollIntoView()
 }))
@@ -10,3 +13,11 @@ courses.forEach(course => course.addEventListener('click', () => {
 details.forEach(detail => detail.addEventListener('click', () => {
   section.scrollIntoView();
 }))
+
+instagram.addEventListener('click', () => {
+  window.open('https://urls.by/QZtRX')
+})
+
+facebook.addEventListener('click', () => {
+  window.open('https://urls.by/OJfye')
+})
